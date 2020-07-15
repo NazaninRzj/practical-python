7e0817b4e6d5c0b9f9fd0643f484fceddd468d6f
Nazanin Rezajooei <nrezajooei@mun.ca>
2020-07-14 23:15:36 -0230

    pcost

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
+
+Total_cost = 0
+with open("Data/portfolio.csv") as f:
+    header = next(f)
+    for line in f:
+        row = line.split(",")
+        price = float(row[2])
+        share = float(row[1])
+        Total_cost += (price * share)
+print("Total_cost= ", Total_cost)
