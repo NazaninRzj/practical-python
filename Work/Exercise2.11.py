ee62f41f0d183e66cb7e1e8e21d599fc4032f83b
Nazanin Rezajooei <nrezajooei@mun.ca>
2020-07-17 18:29:28 -0230

    Exercise 2.11

---
+++ Work/Exercise2.11.py
@@ -0,0 +1,47 @@
+import csv
+
+def read_portfolio(filename):
+    portfolio = []
+    total = 0
+    with open(filename) as f:
+        rows = csv.reader(f)
+        header = next(rows)
+        for row in rows:
+            stock = {'name': row[0],
+           'shares' : int(row[1]),
+           'price' : float(row[2])}
+            portfolio.append(stock)
+        for r in portfolio:
+            total += r['shares'] * r['price']
+        return portfolio
+
+def read_prices(filename):
+    prices = {}
+    with open(filename) as f:
+        rows = csv.reader(f)
+        for row in rows:
+            if len(row) == 2:
+                prices[row[0]] = float(row[1])
+    return prices
+
+
+def make_report(portofilio, prices):
+    summary = []
+    for stock in portofilio:
+        current_price = prices[stock['name']]
+        # print(stock['name'],current_price)
+        change = round(current_price - stock['price'], 2)
+        # print(change)
+        s = (stock['name'], stock['shares'], current_price, change)
+        summary.append(s)
+    return summary
+
+portofilio = (read_portfolio("Data/portfolio.csv"))
+prices = (read_prices("Data/prices.csv"))
+report = (make_report(portofilio, prices))
+
+headers = ('Name', 'Shares', 'Price', 'Change')
+print('%10s %10s %10s %10s' %headers)
+print(('-'*10 + ' ') * len(headers))
+for r in report:
+    print('%10s %10d %10.2f %10.2f' %r)
