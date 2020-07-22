416faa5f5adb5eec4a948a278562b1763b6b1bce
Nazanin Rezajooei <nrezajooei@mun.ca>
2020-07-22 02:03:04 -0230

    Exercise3.3

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,20 @@
 # fileparse.py
 #
 # Exercise 3.3
+import csv
+from pprint import pprint
+def parse_csv(filename):
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        records = []
+        for row in rows:
+            if not row:
+                continue
+            record = dict(zip(headers, row))
+            records.append(record)
+
+    return records
+
+portfolio = parse_csv('Data/portfolio.csv')
+# pprint(portfolio)
