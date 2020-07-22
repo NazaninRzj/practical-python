a5de8b1e1af560ad3ee52c86b9894d4b0c588671
Nazanin Rezajooei <nrezajooei@mun.ca>
2020-07-22 02:04:56 -0230

    Exercise 16_2

---
+++ Work/Exercise3.16_2.py
@@ -0,0 +1,15 @@
+import report_final
+
+def portfolio_cost(filename):
+    portfolio = report_final.read_portfolio(filename)
+    return sum([s['shares']*s['price'] for s in portfolio])
+
+def main(args):
+    if len(args) != 2:
+        raise SystemExit('Usage: %s portfoliofile' % args[0])
+    filename = args[1]
+    print('Total cost:', portfolio_cost(filename))
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
