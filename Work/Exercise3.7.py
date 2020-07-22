dc2bbfbcbece42ffde313a4df3bfe9ff54966ee0
Nazanin Rezajooei <nrezajooei@mun.ca>
2020-07-22 02:03:52 -0230

    Exercise3.7

---
+++ Work/Exercise3.7.py
@@ -0,0 +1,45 @@
+import csv
+from pprint import pprint
+
+def parse_csv(filename, select=None ,types=None, has_headers=True, delimiter=','):
+
+    with open(filename) as f:
+        rows = csv.reader(f, delimiter = delimiter)
+
+        if has_headers:
+            headers = next(rows)
+        else:
+            headers = []
+
+        if select:
+            indices = [headers.index(colname) for colname in select]
+            headers = select
+
+        records = []
+        for row in rows:
+            if not row:
+                continue
+
+            if select:
+                row = [ row[index] for index in indices ]
+
+            if types:
+                row = [func(val) for func, val in zip(types, row)]
+
+            if headers:
+                record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
+
+            records.append(record)
+
+    return records
+
+
+# shares_held = parse_csv('Data/portfolio.csv', select=['name','shares'], types=[str, int])
+# portfolio = parse_csv('Data/portfolio.csv', types=[str, int, float], has_headers=True)
+# prices = parse_csv('Data/prices.csv', types=[str,float], has_headers=False)
+portfolio = parse_csv('Data/portfolio.dat', types=[str, int, float], delimiter=',')
+# pprint(shares_held)
+pprint(portfolio)
+# pprint(prices)
